7e3b59de14610e7c2cb7c2357a4c57ad8d7f6700
Reedsy2407 <caraplomobaneado@gmail.com>
2025-01-25 04:15:47 -0500

    Agregar actualizarLista y sortearAmigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+
 function agregarAmigo(){
     let añadirAmigo = document.getElementById('amigo').value
     if(añadirAmigo == ''){
@@ -10,8 +11,40 @@ function agregarAmigo(){
         amigos.push(añadirAmigo);
         console.log(amigos);
         limpiarCampo();
+        actualizarLista();
     }
 }
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        let elemento = document.createElement('li');
+        elemento.textContent = amigos[i];
+        lista.appendChild(elemento);
+    }
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = "";
+
+    if (amigos.length === 0) {
+        let mensaje = document.createElement('p');
+        mensaje.textContent = "No agregaste ningún amigo";
+        resultado.appendChild(mensaje);
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+        let amigoSorteado = amigos[indiceAleatorio];
+        console.log(amigoSorteado);
+
+        let mensaje = document.createElement('p');
+        mensaje.textContent = `El amigo sorteado es: ${amigoSorteado}`;
+        resultado.appendChild(mensaje);
+    }
+}
+
 
 function limpiarCampo(){
     document.getElementById("amigo").value = '';
